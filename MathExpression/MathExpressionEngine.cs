ff498080fac87d7adba310844229f58e219a029d
Konstantin Kompanets <konstantin.kompanets@deleteagency.com>
2020-09-22 18:23:56 +0500

    Added tests

---
+++ MathExpression/MathExpressionEngine.cs
@@ -0,0 +1,13 @@
+﻿using I2M.MathExpression.Interfaces;
+using System;
+
+namespace I2M.MathExpression
+{
+    public class MathExpressionEngine : IMathExpressionEngine
+    {
+        public IExpression ParseExpression(ITokenizer tokenizer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
