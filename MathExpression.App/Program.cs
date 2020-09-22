66be3d1097238edfa9d66284540f0ef37e790c4b
Konstantin Kompanets <konstantin.kompanets@deleteagency.com>
2020-09-22 18:27:21 +0500

    Added console app

--- MathExpression.App/Program.cs
+++ MathExpression.App/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace I2M.MathExpression.App
+﻿using I2M.MathExpression.Exceptions;
+using I2M.MathExpression.Helpers;
+using System;
+
+namespace I2M.MathExpression.App
 {
     internal static class Program
     {
         private static void Main()
         {
+            while (true)
+            {
+                Console.WriteLine("Enter expression:");
+
+                var expression = Console.ReadLine();
+
+                if (expression != null && expression.Equals("exit", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+
+                try
+                {
+                    var result = MathExpressionHelper.Parse(expression).Eval();
+
+                    Console.WriteLine($"Your result: {result}");
+                }
+                catch (ParseException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
     }
 }
