0c29fd83831df259a568cfb8d2db345e06c078ab
Konstantin Kompanets <konstantin.kompanets@deleteagency.com>
2020-09-23 22:05:40 +0500

    Added new inline data

--- MathExpression.Tests/MathExpressionHelperTest.cs
+++ MathExpression.Tests/MathExpressionHelperTest.cs
@@ -13,18 +13,19 @@ namespace I2M.MathExpression.Tests
         [InlineData("-10", -10)]
         [InlineData("10 + 20", 30)]
         [InlineData("10 - 20", -10)]
-        [InlineData("10 + 20 + 30", 60)]
         [InlineData("10 * 20", 200)]
         [InlineData("10 / 20", 0.5)]
         [InlineData("1 + 1 * 10", 11)]
         [InlineData("10.5 + 10.5", 21)]
         [InlineData("10 * 20 / 2", 100)]
         [InlineData("10 - 20 * 2", -30)]
+        [InlineData("10 + 20 + 30", 60)]
         [InlineData("(10 * 20) / 2", 100)]
         [InlineData("10 + (20 / 2)", 20)]
         [InlineData("10 - (20 / 2)", 0)]
         [InlineData("-(10 + (20 / 2))", -20)]
         [InlineData("-((10 + 20) / 2)", -15)]
+        [InlineData("1 + 1 + 1 + 1 * 10", 13)]
         public void ParseExpression_ValidExpression_ReturnsExpectedResult(string value, double expected)
         {
             // Arrange
