88b5d172c4337872fff07aebd8068fd9be5efeea
Konstantin Kompanets <konstantin.kompanets@deleteagency.com>
2020-10-06 20:56:09 +0500

    Changed test name

--- MathExpression.Tests/MathExpressionEngineTest.cs
+++ MathExpression.Tests/MathExpressionEngineTest.cs
@@ -10,7 +10,7 @@ namespace I2M.MathExpression.Tests
     public class MathExpressionEngineTest
     {
         [Fact]
-        public void ParseExpression_ExpressionString_ReturnsExpectedEval()
+        public void ParseExpression_ValidExpression_ReturnsExpectedEval()
         {
             // Arrange
             var tokens = new Queue<Token>(new[]
