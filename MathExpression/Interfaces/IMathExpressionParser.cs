8e363345e91fc6c3db6d51ddd120d92913866b44
Konstantin Kompanets <konstantin.kompanets@deleteagency.com>
2020-10-08 00:49:44 +0500

    Added expression parser

---
+++ MathExpression/Interfaces/IMathExpressionParser.cs
@@ -0,0 +1,7 @@
+﻿namespace I2M.MathExpression.Interfaces
+{
+    public interface IMathExpressionParser
+    {
+        IExpression Parse(ITokenizer tokenizer);
+    }
+}
--- MathExpression/MathExpressionEngine.cs
+++ MathExpression/MathExpressionEngine.cs
@@ -1,9 +1,6 @@
-﻿using I2M.MathExpression.Exceptions;
-using I2M.MathExpression.Expressions;
-using I2M.MathExpression.Extensions;
+﻿using I2M.MathExpression.Extensions;
 using I2M.MathExpression.Interfaces;
 using System;
-using System.Linq;
 
 namespace I2M.MathExpression
 {
@@ -22,120 +19,13 @@ namespace I2M.MathExpression
 
             tokenizer.Init();
 
-            var expression = ParseLowPriority(tokenizer);
+            var expressionParser = MathExpressionParser.CreateParser(_operationFactory);
+
+            var expression = expressionParser.Parse(tokenizer);
 
             tokenizer.CurrentToken.EnsureEndOfFileSymbol();
 
             return expression;
-        }
-
-        private IExpression ParseLowPriority(ITokenizer tokenizer)
-        {
-            var leftExpression = ParseHighPriority(tokenizer);
-
-            while (true)
-            {
-                var operation = _operationFactory.CreateLowPriorityOperation(tokenizer.CurrentToken.Symbols.First());
-
-                if (operation == null) return leftExpression;
-
-                tokenizer.NextToken();
-
-                var rightExpression = ParseHighPriority(tokenizer);
-
-                leftExpression = new BinaryExpression(leftExpression, rightExpression, operation);
-            }
-        }
-
-        private IExpression ParseHighPriority(ITokenizer tokenizer)
-        {
-            var leftExpression = ParseUnary(tokenizer);
-
-            while (true)
-            {
-                var operation = _operationFactory.CreateHighPriorityOperation(tokenizer.CurrentToken.Symbols.First());
-
-                if (operation == null) return leftExpression;
-
-                tokenizer.NextToken();
-
-                var rightExpression = ParseUnary(tokenizer);
-
-                leftExpression = new BinaryExpression(leftExpression, rightExpression, operation);
-            }
-        }
-
-        private IExpression ParseUnary(ITokenizer tokenizer)
-        {
-            while (true)
-            {
-                if (IsPositiveTokenType(tokenizer)) continue;
-                if (TryGetNegativeExpression(tokenizer, out var negativeExpression)) return negativeExpression;
-                if (TyeGetNumberExpression(tokenizer, out var numberExpression)) return numberExpression;
-                if (TryGetBracketExpression(tokenizer, out var bracketExpression)) return bracketExpression;
-
-                throw new ExpressionParseException($"Unexpected symbol: {tokenizer.CurrentToken.Symbols.First()}");
-            }
-        }
-
-        private static bool IsPositiveTokenType(ITokenizer tokenizer)
-        {
-            if (tokenizer.CurrentToken.IsAddSymbol())
-            {
-                tokenizer.NextToken();
-
-                return true;
-            }
-
-            return false;
-        }
-
-        private bool TryGetNegativeExpression(ITokenizer tokenizer, out IExpression negativeExpression)
-        {
-            negativeExpression = null;
-
-            if (tokenizer.CurrentToken.IsSubtractSymbol())
-            {
-                tokenizer.NextToken();
-
-                var rightExpression = ParseUnary(tokenizer);
-
-                negativeExpression = new UnaryExpression(rightExpression, a => -a);
-            }
-
-            return negativeExpression != null;
-        }
-
-        private static bool TyeGetNumberExpression(ITokenizer tokenizer, out IExpression numberExpression)
-        {
-            numberExpression = null;
-
-            if (tokenizer.CurrentToken.Number.HasValue)
-            {
-                numberExpression = new NumberExpression(tokenizer.CurrentToken.Number.Value);
-
-                tokenizer.NextToken();
-            }
-
-            return numberExpression != null;
-        }
-
-        private bool TryGetBracketExpression(ITokenizer tokenizer, out IExpression bracketExpression)
-        {
-            bracketExpression = null;
-
-            if (tokenizer.CurrentToken.IsLeftBracketSymbol())
-            {
-                tokenizer.NextToken();
-
-                bracketExpression = ParseLowPriority(tokenizer);
-
-                tokenizer.CurrentToken.EnsureRightBracketSymbol();
-
-                tokenizer.NextToken();
-            }
-
-            return bracketExpression != null;
         }
     }
 }
